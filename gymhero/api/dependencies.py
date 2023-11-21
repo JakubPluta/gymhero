62d9ff2888d7b3c53c662d402bf59c668960ab56
jp <plutakuba@gmail.com>
2023-11-21 20:39:48 +0100

    docs: added docstrings to user crud module

--- gymhero/api/dependencies.py
+++ gymhero/api/dependencies.py
@@ -19,6 +19,16 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login/token")
 def get_pagination_params(
     skip: int = Query(0, ge=0), limit: int = Query(10, gt=0)
 ) -> Tuple[int, int]:
+    """
+    Get the pagination parameters.
+
+    Args:
+        skip (int): The number of items to skip. Defaults to 0.
+        limit (int): The maximum number of items to return. Defaults to 10.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the skip and limit values.
+    """
     return skip, limit
 
 
@@ -26,6 +36,7 @@ def _get_credential_exception(
     status_code: int = status.HTTP_401_UNAUTHORIZED,
     details: str = "Could not validate credentials",
 ) -> HTTPException:
+    """Create an HTTPException with the given status code and details"""
     credentials_exception = HTTPException(
         status_code=status_code,
         detail=details,
@@ -34,20 +45,33 @@ def _get_credential_exception(
     return credentials_exception
 
 
-def get_current_user(
-    db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
-) -> User:
+def get_token(token: str = Depends(oauth2_scheme)) -> TokenPayload:
+    """
+    Retrieve the token payload from the provided JWT token.
+
+    Parameters:
+        token (str, optional): The JWT token. Defaults to the value returned by the `oauth2_scheme` dependency.
+
+    Returns:
+        TokenPayload: The decoded token payload.
+
+    Raises:
+        HTTPException: If there is an error decoding the token or validating the payload.
+    """
     try:
         payload = jwt.decode(
             token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
         )
-        username: str = payload.get("sub")
-        if username is None:
-            raise _get_credential_exception()
         token_data = TokenPayload(**payload)
     except (jwt.JWTError, ValidationError):
-        raise _get_credential_exception()
-    user = user_crud.get_one_record(db, User.id == token_data.sub)
+        raise _get_credential_exception(status_code=status.HTTP_403_FORBIDDEN)
+    return token_data
+
+
+def get_current_user(
+    db: Session = Depends(get_db), token: TokenPayload = Depends(get_token)
+) -> User:
+    user = user_crud.get_one_record(db, User.id == token.sub)
     if user is None:
         raise _get_credential_exception(
             status_code=status.HTTP_404_NOT_FOUND, details="User not found"
--- gymhero/crud/user.py
+++ gymhero/crud/user.py
@@ -9,18 +9,55 @@ from gymhero.security import verify_password
 
 class UserCRUDRepository(CRUDRepository):
     def get_user_by_email(self, db: Session, email: str) -> Optional[User]:
+        """
+        Get a user by email.
+
+        Parameters:
+            db (Session): The database session.
+            email (str): The email of the user.
+
+        Returns:
+            Optional[User]: The user found by email, or None if not found.
+        """
         return self.get_one_record(db, self._model.email == email)
 
     @staticmethod
     def is_super_user(user: User) -> bool:
+        """
+        Check if the given user is a super user.
+
+        Parameters:
+            user (User): The user to check.
+
+        Returns:
+            bool: True if the user is a super user, False otherwise.
+        """
         return user.is_superuser
 
     @staticmethod
     def is_active_user(user: User) -> bool:
+        """
+        Check if a user is active.
+
+        Parameters:
+            user (User): The user object to check.
+
+        Returns:
+            bool: True if the user is active, False otherwise.
+        """
         return user.is_active
 
     @staticmethod
     def deactivate_user(db: Session, user: User) -> User:
+        """Deactivates a user by setting their `is_active` flag to `False`.
+
+        Parameters:
+            db (Session): The database session object.
+            user (User): The user to deactivate.
+
+        Returns:
+            User: The deactivated user object.
+        """
         user.is_active = False
         db.add(user)
         db.commit()
@@ -30,6 +67,17 @@ class UserCRUDRepository(CRUDRepository):
     def authenticate_user(
         self, db: Session, email: str, password: str
     ) -> Optional[User]:
+        """
+        Authenticates a user with the given email and password.
+
+        Parameters:
+            db (Session): The database session object.
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            Optional[User]: The authenticated user if successful, None otherwise.
+        """
         user = self.get_user_by_email(db, email)
         if not user:
             return None
