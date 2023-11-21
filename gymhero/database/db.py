347000d87195e409de21a6e59ce31e3ec6637bd8
jp <plutakuba@gmail.com>
2023-11-21 20:50:37 +0100

    feat: added new alembic migration

--- gymhero/database/db.py
+++ gymhero/database/db.py
@@ -6,6 +6,18 @@ logger = get_logger(__name__)
 
 
 def get_db() -> Generator:
+    """
+    Generate the database session.
+
+    This function returns a generator object that provides a database session. The session is created using the `SessionLocal` class.
+
+    Returns:
+        Generator: A generator that yields the database session.
+
+    Raises:
+        Exception: If an error occurs while creating the session.
+
+    """
     db = SessionLocal()
     try:
         yield db
--- gymhero/models/user.py
+++ gymhero/models/user.py
@@ -5,6 +5,8 @@ from gymhero.database.base_class import Base
 
 
 class User(Base):
+    __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
