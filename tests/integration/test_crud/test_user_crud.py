b9e4110b33befb6d53fbe08e8f0713d46e826eed
jp <plutakuba@gmail.com>
2023-12-08 15:52:17 +0100

    feat: users crud tests added

--- tests/integration/test_crud/test_user_crud.py
+++ tests/integration/test_crud/test_user_crud.py
@@ -1,8 +1,8 @@
 import pytest
-
+from sqlalchemy.exc import IntegrityError
 from gymhero.crud import user_crud
 from gymhero.models.user import User
-from gymhero.schemas.user import UserCreate, UserInDB
+from gymhero.schemas.user import UserCreate, UserInDB, UserUpdate
 from gymhero.security import get_password_hash
 from scripts.core._initsu import seed_superuser
 
@@ -64,3 +64,35 @@ def test_can_create_user(get_test_db):
         and user.is_superuser is False
         and user.hashed_password != user_create.password
     )
+
+
+def test_should_not_create_user_if_email_in_use(get_test_db):
+    """Test that we can create a user"""
+    user = user_crud.get_user_by_email(get_test_db, "testuser@mail.com")
+    assert user is not None
+
+    user_create = UserCreate(email="testuser@mail.com", password="password")
+    user_in = UserInDB(
+        **user_create.model_dump(),
+        hashed_password=get_password_hash(user_create.password),
+    )
+    with pytest.raises(IntegrityError):
+        user_crud.create(get_test_db, user_in)
+
+
+def test_should_update_user(get_test_db):
+    """Test that we can update a user"""
+    user = user_crud.get_one(get_test_db, User.id == 1)
+    name_before = user.full_name
+    user_update = UserUpdate(full_name="Test Test")
+    user = user_crud.update(get_test_db, user, user_update)
+    assert user.full_name == "Test Test"
+    assert user.full_name != name_before
+
+
+def test_should_delete_user(get_test_db):
+    """Test that we can delete a user"""
+    user = user_crud.get_one(get_test_db, User.id == 1)
+    user_crud.delete(get_test_db, user)
+    user = user_crud.get_one(get_test_db, User.id == 1)
+    assert user is None
